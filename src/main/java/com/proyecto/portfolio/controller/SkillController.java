3eaebd405599643ee3363eecf0b0d05a662ed6da
aletriste <atrsitezza@gmail.com>
2022-07-31 03:02:02 -0300

    actualizado cross origins

--- src/main/java/com/proyecto/portfolio/controller/SkillController.java
+++ src/main/java/com/proyecto/portfolio/controller/SkillController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://pagina-portfolio-v3.web.app/")
 public class SkillController {
     @Autowired
     public ISkillService skillServ;
