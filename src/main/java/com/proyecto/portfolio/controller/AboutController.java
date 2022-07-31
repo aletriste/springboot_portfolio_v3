92ca7c17f22cc06ef68ea94e21fd16423305c088
aletriste <atrsitezza@gmail.com>
2022-07-31 01:11:42 -0300

    actualizado cross origins

--- src/main/java/com/proyecto/portfolio/controller/AboutController.java
+++ src/main/java/com/proyecto/portfolio/controller/AboutController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://pagina-portfolio-v3.web.app")
 public class AboutController {
     
      @Autowired
